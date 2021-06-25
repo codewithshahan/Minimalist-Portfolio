3eba670a55e84edff154caafedbcb0d79455b5fd
Programming with Shahan <programmingwithshahan@gmail.com>
2021-06-25 11:03:37 +0600

    Add files via upload
    Completed code of minimalist portfolio website. Reuse allowed and I will be grateful if you can refer to my name or youtube channel when sharing with others!
    Coool.....

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".hero-container");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    container.classList.toggle("active");
+  });
+});
